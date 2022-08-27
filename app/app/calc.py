bfc3269544da6997e2ef25a868a3241c66fdfb92
Jude <jisitua@gmail.com>
2022-08-26 20:36:07 -0600

    Pushing unit test, postgres psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+Calculates two integers and returns the result.
+"""
+
+from calendar import c
+
+
+def add(a,b):
+    return a + b
+
+def subtractNum(a,b):
+    """Subtracting two numbers"""
+    return b - a
+
+def distdistinct_val(a):
+    return list(set(a))
---
+++ app/app/tests.py
@@ -0,0 +1,26 @@
+"""
+Unit testing
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class Add_integers (SimpleTestCase):
+
+    def test_addition(self):
+        """Test number addition"""
+        res = calc.add(9,3)
+
+        self.assertEqual(res, 12)
+
+    def test_subtraction(self):
+        """Test number subtraction"""
+        res = calc.subtractNum(5,10)
+
+        self.assertEqual(res, 5)
+    
+    def test_distinct_vales(self):
+        """Testing distinct values"""
+        res = calc.distdistinct_val([1,4,3,4,3,4,5])
+        
+        self.assertEqual(res, [1,3,4,5])
