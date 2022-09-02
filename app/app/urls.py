445666ac69e473cc8b776169e4a466ee797e0004
Jude <jisitua@gmail.com>
2022-09-01 23:17:45 -0600

    updated

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,8 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_spectacular.views import (
+    spectacularAPIViews,
+    spectacularSwaggerView,
+)
+
 from django.contrib import admin
 from django.urls import path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
