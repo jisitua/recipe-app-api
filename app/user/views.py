1abe25a8be30ca704e4bea2d23e88c845af0644d
Jude <jisitua@gmail.com>
2022-09-03 06:16:43 -0600

    User models

---
+++ app/user/views.py
@@ -0,0 +1,9 @@
+"""
+Views for the users API.
+"""
+from rest_framework import generics
+from user.serializers import UserSerializer
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system using an API POST"""
+    serializer_class = UserSerializer
